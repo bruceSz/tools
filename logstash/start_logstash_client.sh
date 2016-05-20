6b53838da8dc24ca0e2c92e3e7408b21264d8cac
root <root@A01-R06-076-I191-41.JD.LOCAL>
2016-05-20 20:05:18 +0800

    logstash config

---
+++ logstash/start_logstash_client.sh
@@ -0,0 +1,4 @@
+
+PATH=/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/logstash/bin
+JAVA_HOME=/usr/java/jdk1.8.0_91
+/opt/logstash/bin/logstash -f client_config.ini
---
+++ logstash/start_logstash_server.sh
@@ -0,0 +1,3 @@
+PATH=/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/logstash/bin
+JAVA_HOME=/usr/java/jdk1.8.0_91
+/opt/logstash/bin/logstash -f server_config.ini
